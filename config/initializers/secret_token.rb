a1d988f1bd758c8cb1617a13a890b6857d0e14a8
Michael Mayorov <mmajorov@gmail.com>
2014-04-11 17:41:52 +0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SolrNotes::Application.config.secret_key_base = '2e18451c4b285bf9d4daa4fbc9a65d9856a9352965c687c9c5c5492a378b177083784a656b7acad9a4c401cff9c9061b6270f5950bdce6d1a41222dc708b3e20'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SolrNotes::Application.config.session_store :cookie_store, key: '_solr-notes_session'
