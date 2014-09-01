071815c519a9658e0d74083e635c4bd3e1ecd56e
Michael Mayorov <mmajorov@gmail.com>
2014-09-01 18:22:35 +0400

    use pg

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140302194716) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "notes", force: true do |t|
     t.string   "name"
     t.text     "content"
