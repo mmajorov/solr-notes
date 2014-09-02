2775b117db787149addbfadfe4ca5c5233614079
Michael Mayorov <mmajorov@gmail.com>
2014-09-02 18:11:01 +0400

    include capybara dsl for specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,8 +2,9 @@
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
-require 'rspec/autorun'
+#require 'rspec/autorun'
 require 'capybara/rspec'
+require 'capybara/rails'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -40,4 +41,6 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+  #inclide capybara dsl
+  config.include Capybara::DSL
 end
