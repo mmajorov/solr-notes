2c6e19532be3e5360ab6f736da0c19fe0416b7f6
Michael Mayorov <mmajorov@gmail.com>
2014-09-05 11:59:18 +0400

    generate category model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140302194716) do
+ActiveRecord::Schema.define(version: 20140905075834) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: true do |t|
+    t.string   "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "notes", force: true do |t|
     t.string   "name"
