65ca3f2fa7659053bf8d18a05a20793e6a95919b
Michael Mayorov <mmajorov@gmail.com>
2014-09-09 16:38:30 +0400

    categories scaffold and select in notes form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 SolrNotes::Application.routes.draw do
+
   root 'notes#index'
   resources :notes
+  resources :categories
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
